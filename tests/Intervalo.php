26c4c063b322b1f1b263607454b132f0ad2e9baa
Adrianny <a.ingridreis@gmail.com>
2017-07-31 20:51:04 -0300

    case 1 fail

---
+++ tests/Intervalo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+class Intervalo
+{
+    public static function intervalo($lista)
+    {
+
+    }
+}
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,16 +5,16 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\Intervalo;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+  // case 1 - lista > 0 (true)
+  public function testCase1()
+  {
+      $lista = [10, 11, 12, 20, 21, 22];
+      $result = Intervalo::intervalo($lista);
+      $this->assertTrue($result);
+  }
+
 }
